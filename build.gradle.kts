1ae5979b85645afa46e41755e931497ef0b02245
Vũ Thành Luân <vtluan191123@gmail.com>
2025-05-30 16:24:53 +0700

    Update build.gradle.kts cấu hình cho thư viện và JitPack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,38 @@
 plugins {
+	`java-library`
+	`maven-publish`
 	java
 	id("org.springframework.boot") version "3.5.0"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("java-library")
 }
 
-group = "com.vtluan.demo"
-version = "0.0.2-SNAPSHOT"
+tasks.bootJar {
+	enabled = false
+}
+
+tasks.jar {
+	enabled = true
+	archiveClassifier = ""
+}
+
+val groupId = "com.github.vtluan191123"
+val artifactId = "test_lib"
+val version = "1.0.0"
+
+group = groupId
+this.version = version
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
+		languageVersion.set(JavaLanguageVersion.of(17))
 	}
+	withSourcesJar()
+	withJavadocJar()
 }
 
 repositories {
 	mavenCentral()
+	maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
@@ -26,4 +43,29 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+publishing {
+	publications {
+		create<MavenPublication>("mavenJava") {
+			from(components["java"])
+			groupId = groupId
+			artifactId = artifactId
+			version = version
+		}
+	}
+	repositories {
+		// Thường không cần cấu hình repository này nếu chỉ dùng JitPack (JitPack tự build)
+		// Nhưng bạn có thể để nếu cần publish trực tiếp
+		maven {
+			url = uri("https://jitpack.io")
+		}
+	}
+}
+
+tasks.javadoc {
+	options {
+		// Tắt cảnh báo thiếu javadoc khi build
+		(this as StandardJavadocDocletOptions).addStringOption("Xdoclint:all,-missing", "-quiet")
+	}
 }
